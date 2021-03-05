9c0beac7bbb6f87304458079a299746e308c3af3
MICHEL <michelrvieira@gmail.com>
2021-03-05 20:58:17 -0300

    Inclusão dos arquivos do projeto
    Inclusão dos arquivos do projeto que estavam em outro diretório, na minha máquina local.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* document.addEventListener('click', function() {
+    
+    const mainHeading = document.querySelector('h1');
+
+    mainHeading.style.backgroundColor = 'red';
+
+} ) */
+
+document.addEventListener('click', function(event) {
+    console.log(event);
+})
